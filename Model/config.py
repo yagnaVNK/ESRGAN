049c3446dc6c4427e6fb4e83c52882ec03326e1e
yagnaVNK <yagna.kaasaragadda@gmail.com>
2024-03-25 20:24:07 -0400

    Added humans data

--- Model/config.py
+++ Model/config.py
@@ -10,10 +10,10 @@ CHECKPOINT_DISC = "disc.pth"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 LEARNING_RATE = 1e-4
 NUM_EPOCHS = 100
-BATCH_SIZE = 8
+BATCH_SIZE = 16
 LAMBDA_GP = 10
-NUM_WORKERS = 1
-HIGH_RES = 512
+NUM_WORKERS = 4
+HIGH_RES = 128
 LOW_RES = HIGH_RES // 4
 IMG_CHANNELS = 3
 
